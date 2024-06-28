2f1bc12522cfacdef7c32fa2a2a000b3c9ecb9c9
FelipeCardona95 <cardonapechuga@gmail.com>
2024-06-27 19:05:05 -0500

    updating

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,17 @@
 FROM node:latest
 
+# Install dependencies for canvas
+RUN apt-get update && apt-get install -y \
+    libcairo2-dev \
+    libpango1.0-dev \
+    libjpeg-dev \
+    libgif-dev \
+    librsvg2-dev
+
 COPY . /usr/src/app
 
 WORKDIR /usr/src/app
 
 RUN npm install
 
-CMD ["npm","start"]
+CMD ["npm", "start"]
--- ao-killbot.js
+++ ao-killbot.js
@@ -9,123 +9,116 @@
 const config = require('./config.json');
 
 // Require modules
-const Discord = require('discord.js');
-const request = require('request');
+const { Client, GatewayIntentBits } = require('discord.js');
+const axios = require('axios');
 
-if (typeof Discord !== 'undefined') {
-    const client = new Discord.Client();
-}
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
 
 var lastRecordedKill = -1;
 
-if (typeof config !== 'undefined') {
-    // Convert all player names to lowercase
-    var playerNames = [];
-    for (var i = 0; i < config.players.length; i++) {
-        playerNames.push(config.players[i].toLowerCase())
+var playerNames = [];
+for (var i = 0; i < config.players.length; i++) {
+    playerNames.push(config.players[i].toLowerCase());
+}
+
+async function fetchKills(limit = 51, offset = 0) {
+    try {
+        const response = await axios.get(`https://gameinfo.albiononline.com/api/gameinfo/events?limit=${limit}&offset=${offset}`);
+        parseKills(response.data);
+    } catch (error) {
+        console.error('Error fetching kills:', error);
     }
 }
 
-
-/**
- * Fetch recent kills from the Gameinfo API
- * @param  {Number} limit  [max kills to get]
- * @param  {Number} offset [offset for first kill]
- * @return {json} [json array of events]
- */
-function fetchKills(limit = 51, offset = 0) {
-    request({
-        uri: 'https://gameinfo.albiononline.com/api/gameinfo/events?limit=' + limit + '&offset=' + offset,
-        json: true
-    }, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            parseKills(body);
-        } else {
-            console.log('Error: ', error); // Log the error
-        }
-    });
-}
-
-/**
- * Parse returned JSON from Gameinfo to
- * find alliance members on the killboard
- * @param  {json} events
- */
 function parseKills(events) {
     var count = 0;
     var breaker = lastRecordedKill;
 
     events.some(function (kill, index) {
-        // Save the most recent kill for tracking
-        if (index == 0) {
+        if (index === 0) {
             lastRecordedKill = kill.EventId;
         }
 
-        // Don't process data for the breaker KILL
-        if (kill.EventId != breaker) {
-            if (kill.Killer.AllianceName.toLowerCase() == config.allianceName.toLowerCase() || kill.Victim.AllianceName.toLowerCase() == config.allianceName.toLowerCase()) {
-                // Alliance KILL
+        if (kill.EventId !== breaker) {
+            if (kill.Killer.AllianceName.toLowerCase() === config.allianceName.toLowerCase() || kill.Victim.AllianceName.toLowerCase() === config.allianceName.toLowerCase()) {
                 postKill(kill);
-            } else if (kill.Killer.GuildName.toLowerCase() == config.guildName.toLowerCase() || kill.Victim.GuildName.toLowerCase() == config.guildName.toLowerCase()) {
-                // Guild Kill
+            } else if (kill.Killer.GuildName.toLowerCase() === config.guildName.toLowerCase() || kill.Victim.GuildName.toLowerCase() === config.guildName.toLowerCase()) {
                 postKill(kill);
             } else if (playerNames.includes(kill.Killer.Name.toLowerCase()) || playerNames.includes(kill.Victim.Name.toLowerCase())) {
-                // Player kill
                 postKill(kill);
             }
         } else {
             count++;
         }
 
-        return kill.EventId == breaker;
+        return kill.EventId === breaker;
     });
-
-    // console.log('- Skipped ' + count + ' kills');
 }
 
-function postKill(kill, channel = config.botChannel) {
-    //quick fix to not post kills with 0 fame (like arena kills after the patch)
-    if (kill.TotalVictimKillFame == 0) {
+function getEquipmentImageUrl(equipment) {
+    return equipment && equipment.Type ? `https://render.albiononline.com/v1/item/${equipment.Type}.png` : 'https://albiononline.com/assets/images/killboard/kill__date.png';
+}
+
+function truncateText(text, maxLength) {
+    return text.length > maxLength ? text.substring(0, maxLength - 3) + '...' : text;
+}
+
+async function postKill(kill, channel = config.botChannel) {
+    if (kill.TotalVictimKillFame === 0) {
         return;
     }
 
     var victory = false;
-    if (kill.Killer.AllianceName.toLowerCase() == config.allianceName.toLowerCase() ||
-        kill.Killer.GuildName.toLowerCase() == config.guildName.toLowerCase() ||
+    if (kill.Killer.AllianceName.toLowerCase() === config.allianceName.toLowerCase() ||
+        kill.Killer.GuildName.toLowerCase() === config.guildName.toLowerCase() ||
         config.players.includes(kill.Killer.Name.toLowerCase())) {
         victory = true;
     }
 
-    var assistedBy = "";
-    if (kill.numberOfParticipants == 1) {
-        var soloKill = [
-            'All on their own',
-            'Without assitance from anyone',
-            'All by himself',
-            'SOLO KILL'
-        ];
-        assistedBy = soloKill[Math.floor(Math.random() * soloKill.length)];
-    } else {
-        var assists = [];
-        kill.Participants.forEach(function (participant) {
-            if (participant.Name != kill.Killer.Name) {
-                assists.push(participant.Name);
-            }
-        })
-        assistedBy = "Assisted By: " + assists.join(', ');
-    }
+    var killerItem = kill.Killer.Equipment;
+    var victimItem = kill.Victim.Equipment;
 
-    itemCount = 0;
-    kill.Victim.Inventory.forEach(function (inventory) {
-        if (inventory !== null) {
-            itemCount++;
-        }
-    });
+    var killerDetails = `
+**Name:** ${kill.Killer.Name}
+**Guild:** ${kill.Killer.GuildName || 'None'}
+**Alliance:** ${kill.Killer.AllianceName || 'None'}
+**Item Power:** ${Math.round(kill.Killer.AverageItemPower)}
+**Equipment:**
+- **MainHand:** [${killerItem.MainHand ? killerItem.MainHand.Type : 'None'}](${getEquipmentImageUrl(killerItem.MainHand)})
+- **OffHand:** [${killerItem.OffHand ? killerItem.OffHand.Type : 'None'}](${getEquipmentImageUrl(killerItem.OffHand)})
+- **Head:** [${killerItem.Head ? killerItem.Head.Type : 'None'}](${getEquipmentImageUrl(killerItem.Head)})
+- **Armor:** [${killerItem.Armor ? killerItem.Armor.Type : 'None'}](${getEquipmentImageUrl(killerItem.Armor)})
+- **Shoes:** [${killerItem.Shoes ? killerItem.Shoes.Type : 'None'}](${getEquipmentImageUrl(killerItem.Shoes)})
+- **Cape:** [${killerItem.Cape ? killerItem.Cape.Type : 'None'}](${getEquipmentImageUrl(killerItem.Cape)})
+- **Bag:** [${killerItem.Bag ? killerItem.Bag.Type : 'None'}](${getEquipmentImageUrl(killerItem.Bag)})
+- **Mount:** [${killerItem.Mount ? killerItem.Mount.Type : 'None'}](${getEquipmentImageUrl(killerItem.Mount)})
+- **Potion:** [${killerItem.Potion ? killerItem.Potion.Type : 'None'}](${getEquipmentImageUrl(killerItem.Potion)})
+- **Food:** [${killerItem.Food ? killerItem.Food.Type : 'None'}](${getEquipmentImageUrl(killerItem.Food)})
+`;
 
-    var itemsDestroyedText = "";
-    if (itemCount > 0) {
-        itemsDestroyedText = " destroying " + itemCount + " items";
-    }
+    var victimDetails = `
+**Name:** ${kill.Victim.Name}
+**Guild:** ${kill.Victim.GuildName || 'None'}
+**Alliance:** ${kill.Victim.AllianceName || 'None'}
+**Item Power:** ${Math.round(kill.Victim.AverageItemPower)}
+**Equipment:**
+- **MainHand:** [${victimItem.MainHand ? victimItem.MainHand.Type : 'None'}](${getEquipmentImageUrl(victimItem.MainHand)})
+- **OffHand:** [${victimItem.OffHand ? victimItem.OffHand.Type : 'None'}](${getEquipmentImageUrl(victimItem.OffHand)})
+- **Head:** [${victimItem.Head ? victimItem.Head.Type : 'None'}](${getEquipmentImageUrl(victimItem.Head)})
+- **Armor:** [${victimItem.Armor ? victimItem.Armor.Type : 'None'}](${getEquipmentImageUrl(victimItem.Armor)})
+- **Shoes:** [${victimItem.Shoes ? victimItem.Shoes.Type : 'None'}](${getEquipmentImageUrl(victimItem.Shoes)})
+- **Cape:** [${victimItem.Cape ? victimItem.Cape.Type : 'None'}](${getEquipmentImageUrl(victimItem.Cape)})
+- **Bag:** [${victimItem.Bag ? victimItem.Bag.Type : 'None'}](${getEquipmentImageUrl(victimItem.Bag)})
+- **Mount:** [${victimItem.Mount ? victimItem.Mount.Type : 'None'}](${getEquipmentImageUrl(victimItem.Mount)})
+- **Potion:** [${victimItem.Potion ? victimItem.Potion.Type : 'None'}](${getEquipmentImageUrl(victimItem.Potion)})
+- **Food:** [${victimItem.Food ? victimItem.Food.Type : 'None'}](${getEquipmentImageUrl(victimItem.Food)})
+`;
 
     var embed = {
         color: victory ? 0x008000 : 0x800000,
@@ -134,119 +127,98 @@ function postKill(kill, channel = config.botChannel) {
             icon_url: victory ? 'https://i.imgur.com/CeqX0CY.png' : 'https://albiononline.com/assets/images/killboard/kill__date.png',
             url: 'https://albiononline.com/en/killboard/kill/' + kill.EventId
         },
-        title: assistedBy + itemsDestroyedText,
-        description: 'Gaining ' + kill.TotalVictimKillFame + ' fame',
+        title: "Kill Details",
+        description: `**Fame:** ${dFormatter(kill.TotalVictimKillFame)}`,
         thumbnail: {
-            url: (kill.Killer.Equipment.MainHand.Type ? 'https://gameinfo.albiononline.com/api/gameinfo/items/' + kill.Killer.Equipment.MainHand.Type + '.png' : 'https://albiononline.com/assets/images/killboard/kill__date.png')
+            url: getEquipmentImageUrl(killerItem.MainHand)
         },
-        timestamp: kill.TimeStamp,
-        fields: [{
-                name: "Killer Guild",
-                value: (kill.Killer.AllianceName ? "[" + kill.Killer.AllianceName + "] " : '') + (kill.Killer.GuildName ? kill.Killer.GuildName : '<none>'),
+        fields: [
+            {
+                name: "Killer",
+                value: truncateText(killerDetails, 1024),
                 inline: true
             },
             {
-                name: "Victim Guild",
-                value: (kill.Victim.AllianceName ? "[" + kill.Victim.AllianceName + "] " : '') + (kill.Victim.GuildName ? kill.Victim.GuildName : '<none>'),
+                name: "Victim",
+                value: truncateText(victimDetails, 1024),
                 inline: true
-            },
-            {
-                name: "Killer IP",
-                value: kill.Killer.AverageItemPower.toFixed(2),
-                inline: true
-            },
-            {
-                name: "Victim IP",
-                value: kill.Victim.AverageItemPower.toFixed(2),
-                inline: true
-            },
+            }
         ],
+        timestamp: new Date(kill.TimeStamp).toISOString(),
         footer: {
             text: "Kill #" + kill.EventId
         }
     };
 
-    console.log(embed);
-
-    client.channels.get(channel).send({
-        embed: embed
-    });
-}
-
-/**
- * Wait until ready and logged in
- * If we do not wait for the ready event
- * All commands will process before we are authorized
- */
-if (typeof client !== 'undefined') {
-    client.on('ready', () => {
-        console.log('Ready and waiting!');
-
-        // If the config.username differs, change it
-        if (client.user.username != config.username) {
-            client.user.setUsername(config.username);
-        }
-
-        // Set 'Playing Game' in discord
-        client.user.setActivity(config.playingGame); // broken due to discord API changes
-
-        fetchKills();
-
-        // Fetch kills every 30s
-        var timer = setInterval(function () {
-            fetchKills();
-        }, 30000);
-    });
-}
-
-/**
- * On receive message
- */
-if (typeof client !== 'undefined') {
-    client.on('message', message => {
-        if (message.content.indexOf(config.cmdPrefix) !== 0 || message.author.bot) return;
-        else { // Execute command!
-            var args = message.content.slice(config.cmdPrefix.length).trim().split(/ +/g);
-            var command = args.shift().toLowerCase();
-
-            // Test Command - !ping
-            if (command === 'ping') {
-                message.reply('pong');
-            } else if (command === 'kbinfo') {
-                request({
-                    json: true,
-                    uri: 'https://gameinfo.albiononline.com/api/gameinfo/events/' + args[0]
-                }, function (error, response, body) {
-                    if (!error && response.statusCode === 200) {
-                        postKill(body, message.channel.id);
-                    } else {
-                        console.log('Error: ', error); // Log the error
-                    }
-                });
-            }
-
-            // [ADMIN] - clear config.botChannel messages
-            else if (command === 'kbclear') {
-                if (config.admins.includes(message.author.id) && message.channel.id == config.botChannel) {
-                    message.channel.send('Clearing Killboard').then(msg => {
-                        msg.channel.fetchMessages().then(messages => {
-                            message.channel.bulkDelete(messages);
-                            console.log("[ADMIN] " + message.author.username + " cleared Killboard");
-                        })
-                    })
-                }
-            }
-        }
-    });
-}
-
-if (typeof config !== 'undefined') {
-    if (config.token) {
-        client.login(config.token);
-    } else {
-        console.log("ERROR: No bot token defined")
+    const discordChannel = client.channels.cache.get(channel);
+    if (!discordChannel) {
+        console.error(`Channel ID ${channel} not found in cache.`);
+        return;
     }
-} else {
-    console.log("ERROR: No config file")
-    console.log("execute: cp config.json.example config.json")
+
+    discordChannel.send({ embeds: [embed] });
+}
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Connected to the following servers:`);
+    client.guilds.cache.forEach(guild => {
+        console.log(` - ${guild.name}`);
+    });
+
+    // Check the bot channel
+    const channel = client.channels.cache.get(config.botChannel);
+    if (!channel) {
+        console.error(`Bot channel with ID ${config.botChannel} not found!`);
+    } else {
+        console.log(`Bot will post in channel: ${channel.name}`);
+    }
+
+    // Set 'Playing Game' in discord
+    client.user.setActivity(config.playingGame);
+
+    fetchKills();
+
+    // Fetch kills every 30s
+    var timer = setInterval(function () {
+        fetchKills();
+    }, 30000);
+});
+
+client.on('messageCreate', message => {
+    if (!message.content.startsWith(config.cmdPrefix) || message.author.bot) return;
+
+    const args = message.content.slice(config.cmdPrefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    // Test Command - !ping
+    if (command === 'ping') {
+        message.reply('pong');
+    } else if (command === 'kbinfo') {
+        axios.get(`https://gameinfo.albiononline.com/api/gameinfo/events/${args[0]}`)
+            .then(response => {
+                postKill(response.data, message.channel.id);
+            })
+            .catch(error => {
+                console.error('Error fetching event info:', error);
+            });
+    }
+
+    // [ADMIN] - clear config.botChannel messages
+    else if (command === 'kbclear') {
+        if (config.admins.includes(message.author.id) && message.channel.id === config.botChannel) {
+            message.channel.send('Clearing Killboard').then(msg => {
+                message.channel.messages.fetch().then(messages => {
+                    message.channel.bulkDelete(messages);
+                    console.log(`[ADMIN] ${message.author.username} cleared Killboard`);
+                });
+            });
+        }
+    }
+});
+
+client.login(config.token);
+
+function dFormatter(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") || 0;
 }
