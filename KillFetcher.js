73721e0dfbf9ca4187c6867e1a00e83e0ed68706
pierrefeza <felipecardonagames@gmail.com>
2024-07-06 20:50:04 -0500

    Removing delayed fetch

--- KillFetcher.js
+++ KillFetcher.js
@@ -6,6 +6,7 @@ class KillFetcher {
     this.config = config;
     this.publishedEventIds = new Set(); // Use a Set for efficient storage and lookup
     this.fetchCount = 0; // Counter to track the number of fetches
+    this.isFetching = false;
   }
 
   async fetchKills(retryCount = 0) {
@@ -21,7 +22,7 @@ class KillFetcher {
         console.log(`Fetched ${response.data.length} kills (fetch count: ${this.fetchCount})`);
       }
 
-      // Add a slight delay before the next fetch
+      // Recursive call with a delay
       setTimeout(() => this.fetchKills(), 1000); // 1 second delay
     } catch (error) {
       console.error("Error fetching kills:", error.message);
@@ -30,35 +31,49 @@ class KillFetcher {
         setTimeout(() => this.fetchKills(retryCount + 1), 1000 * (retryCount + 1)); // Exponential backoff
       } else {
         console.error("Max retries reached for fetchKills");
+        // Continue fetching even if max retries reached
+        setTimeout(() => this.fetchKills(), 1000); // 1 second delay
       }
     }
   }
 
-  async fetchKillsDelayed(retryCount = 0) {
-    const offset = 102;
+  async fetchEventsTo(latestEventId, offset = 0, events = []) {
+    if (offset >= 1000) return events;
+
     try {
       const response = await axios.get(
         `https://gameinfo.albiononline.com/api/gameinfo/events?limit=51&offset=${offset}`
       );
-      this.parseDelayedKills(response.data);
+      const albionEvents = response.data;
+      const foundLatest = !albionEvents.every((evt) => {
+        if (!latestEventId) latestEventId = evt.EventId - 1;
+        if (evt.EventId <= latestEventId) return false;
+        if (events.findIndex((e) => e.EventId === evt.EventId) >= 0) return true;
+        events.push(evt);
+        return true;
+      });
 
-      // Log only every 10th fetch to reduce log spam
-      this.fetchCount++;
-      if (this.fetchCount % 10 === 0) {
-        console.log(`Fetched delayed ${response.data.length} kills (fetch count: ${this.fetchCount})`);
-      }
-
-      // Add a slight delay before the next fetch
-      setTimeout(() => this.fetchKillsDelayed(), 1000); // 1 second delay
+      return foundLatest
+        ? events
+        : this.fetchEventsTo(latestEventId, offset + albionEvents.length, events);
     } catch (error) {
-      console.error("Error fetching delayed kills:", error.message);
-      if (retryCount < 5) { // Retry up to 5 times
-        console.log(`Retrying fetchKillsDelayed (${retryCount + 1}/5)`);
-        setTimeout(() => this.fetchKillsDelayed(retryCount + 1), 1000 * (retryCount + 1)); // Exponential backoff
-      } else {
-        console.error("Max retries reached for fetchKillsDelayed");
-      }
+      console.error(`Unable to fetch event data from API: ${error.message}. Retrying...`);
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      return this.fetchEventsTo(latestEventId, offset, events);
     }
+  }
+
+  async fetchAllOffsets() {
+    if (this.isFetching) return;
+    this.isFetching = true;
+
+    const events = await this.fetchEventsTo(null);
+    this.parseKills(events);
+
+    this.isFetching = false;
+
+    // Recursive call with a delay
+    setTimeout(() => this.fetchAllOffsets(), 60000); // 60 seconds delay
   }
 
   parseKills(events) {
@@ -88,33 +103,6 @@ class KillFetcher {
     });
 
     console.log(`Parsed ${events.length} kills, ${this.publishedEventIds.size} unique kills published recently`);
-  }
-
-  parseDelayedKills(events) {
-    // Sort events by kill timestamp before processing
-    events.sort((a, b) => new Date(a.TimeStamp) - new Date(b.TimeStamp));
-
-    events.forEach((kill) => {
-      if (!this.publishedEventIds.has(kill.EventId)) { // Check existence using Set's has method
-        if (
-          kill.Killer.AllianceName.toLowerCase() === this.config.allianceName.toLowerCase() ||
-          kill.Victim.AllianceName.toLowerCase() === this.config.allianceName.toLowerCase() ||
-          kill.Killer.GuildName.toLowerCase() === this.config.guildName.toLowerCase() ||
-          kill.Victim.GuildName.toLowerCase() === this.config.guildName.toLowerCase() ||
-          this.discordBot.playerNames.includes(kill.Killer.Name.toLowerCase()) ||
-          this.discordBot.playerNames.includes(kill.Victim.Name.toLowerCase())
-        ) {
-          console.log(`Delayed posting for event ID: ${kill.EventId}`);
-          this.discordBot.queueKill(kill);
-          this.publishedEventIds.add(kill.EventId); // Add to Set
-
-          // Trim the set to maintain a maximum of 10 recent unique kills
-          if (this.publishedEventIds.size > 10) {
-            this.publishedEventIds.delete(this.publishedEventIds.values().next().value); // Remove oldest element
-          }
-        }
-      }
-    });
   }
 
   async fetchEventById(eventId) {
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const DiscordBot = require("./DiscordBot");
-const KillFetcher = require("./KillFetcher");
+const DiscordBot = require('./DiscordBot');
+const KillFetcher = require('./KillFetcher');
 const config = require("./config.json");
-
 const discordBot = new DiscordBot(config);
-const killFetcher = new KillFetcher(discordBot, config);
+const killFetcher = new KillFetcher(discordBot,config);
 
 discordBot.initialize().then(() => {
   // Initial fetch
   killFetcher.fetchKills();
-  killFetcher.fetchKillsDelayed();
+  killFetcher.fetchAllOffsets();
 });
